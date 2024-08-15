5c328ff67fcfbbbae97359def2cf7df7688f29b0
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2024-08-15 16:32:10 -0400

    Adding GTM tags

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,6 +5,10 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KXH3QS6T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div>
       <Navbar />
       <main>{children}</main>
