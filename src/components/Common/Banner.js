6e5185b8c36ce0a8103b2b60a3124cfdc7532b59
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2024-08-28 17:23:26 -0400

    Copying Heading as a Test

--- src/components/Common/Banner.js
+++ src/components/Common/Banner.js
@@ -31,16 +31,126 @@ const Banner = ({
         }}
       >
         <div
-          sx={{
-            flex: ['1 0 100%', null, '1 0 calc(50% - 1rem)'],
-            maxWidth: ['100%', null, '1 0 calc(50% - 1rem) '],
-
-            order: [2, null, 0],
-            maxHeight: '100px',
-            minHeight: '100%',
+          x={{
+            flex: ['1 0 100%', null, '1 0 50%'],
+            maxWidth: ['100%', null, '50%'],
+            position: 'relative',
+            minHeight: ['100%', null, '100px'],
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1.5rem',
+            px: ['1rem', null, '3rem'],
+            py: ['1rem', null, '3rem'],
+            justifyContent: 'center',
+            alignItems: ['center', null, 'initial'],
           }}
         >
-          
+          <h1
+            sx={{
+              variant: 'text.xxl',
+              lineHeight: 1.15,
+              color: '#FFFFFF',
+              textAlign: ['center', null, 'left'],
+              maxWidth: '600px',
+            }}
+          >
+            {title}
+          </h1>
+          {description && (
+            <p
+              sx={{
+                variant: 'text.normal',
+                lineHeight: '1.5',
+                letterSpacing: '0.09em',
+                color: '#FFFFFF',
+                textAlign: ['center', null, 'left'],
+                maxWidth: '600px',
+              }}
+            >
+              {description}
+            </p>
+          )}{' '}
+          <div
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              gap: '1rem',
+              justifyContent: ['center', null, 'start'],
+              a: {
+                px: '1.5rem',
+                py: '1rem',
+                display: 'inline-block',
+                variant: 'text.xs',
+                borderRadius: '8px',
+                minWidth: '200px',
+                textAlign: 'center',
+                transition: 'all 0.3s',
+              },
+            }}
+          >
+            {ctaPrimary &&
+              (ctaPrimaryLink[0] !== '#' ? (
+                <Link
+                  to={ctaPrimaryLink}
+                  sx={{
+                    background: '#343753',
+                    color: '#000',
+                    '&:hover': {
+                      opacity: 0.75,
+                    },
+                  }}
+                  title={ctaPrimary}
+                >
+                  {ctaPrimary}
+                </Link>
+              ) : (
+                <a
+                  href={ctaPrimaryLink}
+                  sx={{
+                    background: '#343753',
+                    color: '#000',
+                    '&:hover': {
+                      opacity: 0.75,
+                    },
+                  }}
+                  title={ctaPrimary}
+                >
+                  {ctaPrimary}
+                </a>
+              ))}
+            {ctaSecondary &&
+              (ctaSecondaryLink[0] !== '#' ? (
+                <Link
+                  to={ctaSecondaryLink}
+                  sx={{
+                    color: ' #FFFFFF',
+                    border: '1px solid #fff',
+                    '&:hover': {
+                      color: '#47E4E0',
+                      borderColor: '#47E4E0',
+                    },
+                  }}
+                  title={ctaSecondary}
+                >
+                  {ctaSecondary}
+                </Link>
+              ) : (
+                <a
+                  href={ctaSecondaryLink}
+                  sx={{
+                    color: ' #FFFFFF',
+                    border: '1px solid #fff',
+                    '&:hover': {
+                      color: '#47E4E0',
+                      borderColor: '#47E4E0',
+                    },
+                  }}
+                  title={ctaSecondary}
+                >
+                  {ctaSecondary}
+                </a>
+              ))}
         </div>
         <div
           sx={{
