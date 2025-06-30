0b04fec9deee6cfbf468c84fc51d3d2496ca7f98
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2025-06-30 14:14:52 -0400

    Update FeaturedContent.js - July 2025 Update

--- src/components/About/FeaturedContent.js
+++ src/components/About/FeaturedContent.js
@@ -21,64 +21,48 @@ const FeaturedContent = () => {
       }}
     >
       <p>
-        Welcome to Rejoice Fertility. My name is Dr. John David Gordon, and I am so happy to tell you about what sets our practice apart. 
-        I am very excited about what God has in store for us here in Knoxville.
+        Welcome to Rejoice Fertility. My name is Dr. John David Gordon, and I am so happy to tell you about what sets our practice apart. I am very excited about what God has in store for us here in Knoxville.
       </p>
       <p>
-        After practicing fertility medicine in the Metropolitan DC area for 20 years, 
-        my wife and I were encouraged by Dr. Jeffrey Keenan to take a leap of faith in 2019 and relocate here to beautiful East Tennessee. 
-        I left behind a large fertility practice performing over 1500 IVF cycles a year to take over the reins at Southeastern Center for Fertility.
+        After practicing fertility medicine in the Metropolitan DC area for 20 years, my wife and I were encouraged to take a leap of faith in 2019 and relocate here to beautiful East Tennessee. I left behind a large fertility practice performing over 1500 IVF cycles a year to purchase a fertility practice here in Farragut.
       </p>
       <p>
-        My vision was to provide an unparalleled patient experience while making sure patients knew they could discuss their concerns 
-        about the practice of reproductive medicine. To that end, we have succeeded beyond all expectations (and in spite of a world-wide pandemic)!
+        My vision was to provide an unparalleled patient experience while making sure patients knew they could discuss their concerns about the practice of reproductive medicine. To that end, we have succeeded beyond all expectations (and in spite of a world-wide pandemic)!
       </p>
       <p>
-        Since August 2019, we have experienced unprecedented growth. This growth has resulted in the hiring of a multitude of new team members, 
-        increasing both the nursing and the IVF laboratory staff. More and more now, local patients realize they don’t have to travel 
-        beyond Knoxville to receive excellent fertility care.
+        Since August 2019, we have experienced unprecedented growth. This growth has resulted in the hiring of a multitude of new team members, increasing both the nursing and the IVF laboratory staff. More and more now, local patients realize they don’t have to travel beyond Knoxville to receive excellent fertility care.
       </p>
       <p>
-        In addition, patients are now coming here to Knoxville from a multitude of states because they are seeking something 
-        rarely found in reproductive medicine: a clinic that provides excellent technical fertility care while also discussing 
-        the moral, ethical, spiritual and religious concerns raised by IVF. We take care of patients of all faiths and those 
-        with no particular faith or belief in God, but our practice of reproductive medicine is guided by our faith and our understanding
-        of how we are to live out that faith on a day-to-day basis.
+        In addition, patients are now coming here to Knoxville from a multitude of states because they are seeking something rarely found in reproductive medicine: a clinic that provides excellent technical fertility care while also discussing the moral, ethical, spiritual and religious concerns raised by IVF. We take care of patients of all faiths and those with no particular faith or belief in God, but our practice of reproductive medicine is guided by our faith and our understanding of how we are to live out that faith on a day-to-day basis.
       </p>
       <p>
         So what does that mean when it comes to taking care of patients? Here are 4 major differences:
       </p>
       <p>
-        1) We are a “no-discard” IVF program.
-        Any embryos created by a couple are either available for fresh embryo transfer or later frozen embryo transfer, but no viable embryos 
-        will be discarded. As a result, we practice in a very different way than the majority of IVF clinics in the United States. 
-        The only embryo that can never result in a healthy baby is the one you fail to transfer.
+        1) We are a “no-discard” IVF program. Any embryos created by a couple are either available for fresh embryo transfer or later frozen embryo transfer, but no viable embryos will be discarded. As a result, we practice in a very different way than the majority of IVF clinics in the United States. The only embryo that can never result in a healthy baby is the one you fail to transfer.
       </p>
       <p>
         2) We offer couples the option to strictly limit the number of eggs fertilized in IVF. Since we are a “no-discard” IVF program, it is important to discuss, prior to any IVF cycle, the plan regarding how many eggs to fertilize. If a couple really wants to avoid freezing ANY embryos, then we need to limit the number of eggs fertilized to just 1 or 2. Unfertilized eggs can be frozen if desired, but we can certainly decide to transfer all embryos created in an IVF cycle (as long as we fertilize only 1 to 2 eggs).
       </p>
       <p>
-      3) We are trying to put the brakes on the creation of an excessive number of embryos. We perform over 85% of our IVF in natural (unstimulated) cycles or as Mini Stim IVF using oral medications (Clomid or Letrozole) and low doses of fertility shots, with the goal of retrieving 3-8 mature eggs.
+      3) We are trying to put the brakes on the creation of an excessive number of embryos. We perform over 90% of our IVF in natural (unstimulated) cycles or as Mini Stim IVF using oral medications (Clomid or Letrozole) and low doses of fertility shots, with the goal of retrieving 3-8 mature eggs.
       </p>
       <p>
-      4) We are doing what we can to encourage couples to pursue embryo adoption. We have received national and international attention for our work with the National Embryo Donation Center (NEDC). 
+      4) We are doing what we can to encourage couples to pursue embryo adoption. We have received national and international attention for our work on Embryo Adoption.
       <a href="https://www.goodmorningamerica.com/wellness/story/baby-born-27-year-frozen-embryo-breaks-record-74469304"> The births of Molly & Emma Gibson (sisters frozen as embryos for over 20 years) </a>
       and <a href="https://edition.cnn.com/2022/11/21/health/30-year-old-embryos-twins/index.html">Timothy & Lydia Ridgeway (twins frozen as embryos for 30 years) </a>
-      captured the hearts and minds of people across the globe. 
-      For many couples thinking their embryos would never be adopted if they had been frozen for more than a few years, and for those couples who had never heard of embryo adoption, these news stories were eye-opening.
+      captured the hearts and minds of people across the globe. For many couples thinking their embryos would never be adopted if they had been frozen for more than a few years, and for those couples who had never heard of embryo adoption, these news stories were eye-opening.
       </p>
       <p>
-      In 2024 we will be moving into a brand new, state-of-the-art facility built just behind our current office. With the upcoming move to the new office I thought that the time was right for a rebranding; hence, the decision to change the name of the practice from Southeastern Fertility to Rejoice Fertility.
+      In 2024 we moved into a brand new, 10,000 square foot state-of-the-art facility designed and built to provide the best possible experience to our patients.  We love serving the needs of our patients in this beautiful new office while holding fast to the vision that brought my wife and I to East Tennessee in 2019: the ability to provide outstanding medical care to couples experiencing infertility while remaining true to what we are told in the Scriptures.
       </p>
       <p>
-      I am looking forward to serving the needs of our patients in this beautiful new office while holding fast to the vision that brought us to East Tennessee in 2019: the ability to provide outstanding medical care to couples experiencing infertility while remaining true to what we are told in the Scriptures.
+      Come experience a different type of fertility practice…We rejoice in our difference!
       </p>
-      <p>
-      Come experience a different type of fertility practice…We rejoice in that difference!
-      <br></br>
+      <br />
       - John David Gordon, MD
       <br></br>
-      January 2024
+      July 2025
       </p>
       <p>
       <i>"Rejoice in the Lord always; again, I will say, rejoice. Let your reasonableness be known to everyone. The Lord is at hand; do not be anxious about anything, but in everything, by prayer and supplication with thanksgiving, let your requests be made known to God. And the peace of God, which surpasses all understanding, will guard your hearts and your minds in Christ Jesus."  Philippians 4:4-7</i>
