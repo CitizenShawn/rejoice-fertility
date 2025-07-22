773862ee515b45cc10e9a5ffaf5e3c563cf82626
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2025-07-22 10:23:43 -0400

    Update Form.js - Changing to script

--- src/components/Common/Form.js
+++ src/components/Common/Form.js
@@ -45,15 +45,7 @@ const Form = ({ formUrl = 'https://usebasin.com/f/f95fb9d7171b' }) => {
       id="form-section"
     >
         
-    <iframe
-          title="Jotform"
-          src="https://form.jotform.com/251464191705052"
-          style={{ width: '100%', height: '1000px', border: 'none' }}
-          allowFullScreen
-    >
-    </iframe>
-    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
-    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-251464191705052']", "https://form.jotform.com/")</script>
+    <script type="text/javascript" src="https://form.jotform.com/jsform/251464191705052"></script>
     </section>
   );
 };
