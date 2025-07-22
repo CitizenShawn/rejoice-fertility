6d63047781b9bf9597aefd763513a6510cc3b744
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2025-07-22 10:56:06 -0400

    Update Form.js - Replacing with fixed code.

--- src/components/Common/Form.js
+++ src/components/Common/Form.js
@@ -1,72 +1,73 @@
 /** @jsx jsx */
 import * as React from 'react';
 import { jsx } from 'theme-ui';
-import { useForm, ValidationError } from '@formspree/react';
-import { useEffect } from 'react':
+import { useEffect } from 'react';
 
 const Form = () => {
   useEffect(() => {
-    // Dynamically inject external scripts and styles
+    // External scripts needed for JotForm
     const scriptUrls = [
-      "https://cdn.jotfor.ms/s/static/87d70767e6b/static/prototype.forms.js",
-      "https://cdn.jotfor.ms/s/static/87d70767e6b/static/jotform.forms.js",
-      "https://cdn.jotfor.ms/s/static/87d70767e6b/js/punycode-1.4.1.min.js",
-      "https://cdn.jotfor.ms/s/static/87d70767e6b/js/vendor/maskedinput_5.0.9.min.js",
-      "https://cdn.jotfor.ms/s/static/87d70767e6b/js/vendor/smoothscroll.min.js",
-      "https://cdn.jotfor.ms/s/static/87d70767e6b/js/errorNavigation.js",
+      'https://cdn.jotfor.ms/s/static/87d70767e6b/static/prototype.forms.js',
+      'https://cdn.jotfor.ms/s/static/87d70767e6b/static/jotform.forms.js',
+      'https://cdn.jotfor.ms/s/static/87d70767e6b/js/punycode-1.4.1.min.js',
+      'https://cdn.jotfor.ms/s/static/87d70767e6b/js/vendor/maskedinput_5.0.9.min.js',
+      'https://cdn.jotfor.ms/s/static/87d70767e6b/js/vendor/smoothscroll.min.js',
+      'https://cdn.jotfor.ms/s/static/87d70767e6b/js/errorNavigation.js',
     ];
 
-    scriptUrls.forEach(src => {
-      const s = document.createElement('script');
-      s.src = src;
-      s.async = false;
-      document.body.appendChild(s);
+    // Inject scripts sequentially
+    scriptUrls.forEach((src) => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.async = false;
+      document.body.appendChild(script);
     });
 
-    // Insert JotForm config
-    const jotFormScript = document.createElement('script');
-    jotFormScript.innerHTML = `
+    // Inject JotForm initialization config
+    const jotFormInitScript = document.createElement('script');
+    jotFormInitScript.innerHTML = `
       window.enableEventObserver = true;
-      JotForm.newDefaultTheme = true;
-      JotForm.extendsNewTheme = false;
-      JotForm.singleProduct = false;
-      JotForm.newPaymentUIForNewCreatedForms = true;
-      JotForm.texts = ${JSON.stringify({
-        confirmEmail: "E-mail does not match",
-        pleaseWait: "Please wait...",
-        validateEmail: "You need to validate this e-mail",
-        confirmClearForm: "Are you sure you want to clear the form",
-        lessThan: "Your score should be less than or equal to",
-        incompleteFields: "There are incomplete required fields. Please complete them.",
-        required: "This field is required.",
-        // ... you can trim here if too long
-        error: "Error"
-      })};
-      JotForm.newPaymentUI = true;
-      JotForm.originalLanguage = "en";
-      JotForm.isFormViewTrackingAllowed = true;
-      JotForm.replaceTagTest = true;
-      JotForm.activeRedirect = "thanktext";
-      JotForm.uploadServerURL = "https://upload.jotform.com/upload";
-      JotForm.clearFieldOnHide = "disable";
-      JotForm.submitError = "jumpToFirstError";
-      JotForm.isFullSource = true;
-      JotForm.init();
+      if (window.JotForm) {
+        JotForm.newDefaultTheme = true;
+        JotForm.extendsNewTheme = false;
+        JotForm.singleProduct = false;
+        JotForm.newPaymentUIForNewCreatedForms = true;
+        JotForm.texts = ${JSON.stringify({
+          confirmEmail: 'E-mail does not match',
+          pleaseWait: 'Please wait...',
+          validateEmail: 'You need to validate this e-mail',
+          confirmClearForm: 'Are you sure you want to clear the form',
+          lessThan: 'Your score should be less than or equal to',
+          incompleteFields: 'There are incomplete required fields. Please complete them.',
+          required: 'This field is required.',
+          error: 'Error',
+        })};
+        JotForm.newPaymentUI = true;
+        JotForm.originalLanguage = 'en';
+        JotForm.isFormViewTrackingAllowed = true;
+        JotForm.replaceTagTest = true;
+        JotForm.activeRedirect = 'thanktext';
+        JotForm.uploadServerURL = 'https://upload.jotform.com/upload';
+        JotForm.clearFieldOnHide = 'disable';
+        JotForm.submitError = 'jumpToFirstError';
+        JotForm.isFullSource = true;
+        JotForm.init();
+      }
     `;
-    document.body.appendChild(jotFormScript);
+    document.body.appendChild(jotFormInitScript);
 
     // Inject external styles
     const linkUrls = [
-      "https://cdn.jotfor.ms/stylebuilder/static/form-common.css?v=87bd99f",
-      "https://cdn.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?v=3.3.64160",
-      "https://cdn.jotfor.ms/css/styles/payment/payment_styles.css?3.3.64160",
-      "https://cdn.jotfor.ms/css/styles/payment/payment_feature.css?3.3.64160",
+      'https://cdn.jotfor.ms/stylebuilder/static/form-common.css?v=87bd99f',
+      'https://cdn.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?v=3.3.64160',
+      'https://cdn.jotfor.ms/css/styles/payment/payment_styles.css?3.3.64160',
+      'https://cdn.jotfor.ms/css/styles/payment/payment_feature.css?3.3.64160',
     ];
-    linkUrls.forEach(href => {
-      const l = document.createElement('link');
-      l.rel = 'stylesheet';
-      l.href = href;
-      document.head.appendChild(l);
+    linkUrls.forEach((href) => {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = href;
+      document.head.appendChild(link);
     });
   }, []);
 
@@ -79,7 +80,7 @@ const Form = () => {
       }}
       id="form-section"
     >
-      {/* Embed JotForm iframe or raw HTML form */}
+      {/* JotForm HTML inserted dynamically */}
       <form
         className="jotform-form"
         action="https://submit.jotform.com/submit/251464191705052"
@@ -91,7 +92,38 @@ const Form = () => {
         dangerouslySetInnerHTML={{
           __html: `
             <input type="hidden" name="formID" value="251464191705052" />
-            <!-- rest of form fields go here -->
+            <div role="main" class="form-all">
+              <ul class="form-section page-section" role="presentation">
+                <li class="form-line" data-type="control_fullname">
+                  <label class="form-label form-label-top">Name</label>
+                  <div class="form-input-wide">
+                    <input type="text" name="q4_name[first]" placeholder="First Name" />
+                    <input type="text" name="q4_name[last]" placeholder="Last Name" />
+                  </div>
+                </li>
+                <li class="form-line" data-type="control_email">
+                  <label class="form-label form-label-top">Email</label>
+                  <div class="form-input-wide">
+                    <input type="email" name="q5_email" placeholder="example@example.com" />
+                  </div>
+                </li>
+                <li class="form-line" data-type="control_phone">
+                  <label class="form-label form-label-top">Phone Number</label>
+                  <div class="form-input-wide">
+                    <input type="tel" name="q6_phoneNumber[full]" placeholder="(000) 000-0000" />
+                  </div>
+                </li>
+                <li class="form-line" data-type="control_textarea">
+                  <label class="form-label form-label-top">Comments / Questions</label>
+                  <div class="form-input-wide">
+                    <textarea name="q8_comments"></textarea>
+                  </div>
+                </li>
+                <li class="form-line" data-type="control_button">
+                  <button type="submit">Submit</button>
+                </li>
+              </ul>
+            </div>
           `,
         }}
       />
