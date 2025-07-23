f1dafd5aece9b6f834ed2f4d2b144804d7321bdc
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2025-07-23 12:44:56 -0400

    Update Form.js - fixing styling

--- src/components/Common/Form.js
+++ src/components/Common/Form.js
@@ -138,7 +138,7 @@ const Form = () => {
         py: '4rem',
         scrollMarginTop: 100,
         textAlign: 'center',
-        padding: 0,
+        padding: 20,
       }}
       id="form-section"
     >
