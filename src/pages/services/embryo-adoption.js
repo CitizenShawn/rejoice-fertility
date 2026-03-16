a7a19e6c246307c4672aae022288a69005e3ea15
CitizenShawn <119890152+CitizenShawn@users.noreply.github.com>
2026-03-16 16:42:08 -0400

    Update embryo-adoption.js
    Moved FAQ's to appropriate section

--- src/pages/services/embryo-adoption.js
+++ src/pages/services/embryo-adoption.js
@@ -10,6 +10,10 @@ import { Seo } from '../../components/Seo';
 
 const EmbryoAdoptionTestPage = () => {
   const data = [
+    
+
+  ];
+  const q = [
     /*  EMBRYO RECIPIENTS FAQS =================================================== */
  
   {
@@ -439,42 +443,6 @@ question: 'Are there videos and other resources I can read to learn more about e
 answer: 'Yes, in fact it is recommended by the American Society of Reproductive Medicine that you take this step before embryo adoption.  You may do this counseling through any of our affiliates, and as a courtesy to our donors Lauren Wilson, LMSW has offered a discounted rate.  Please contact her directly at <a href="mailto:lauren@flourishcs.com                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    ">lauren@flourishcs.com</a> for more information.',
 
 }
-
-  ];
-  const q = [
-    {
-      question: 'What are the next steps that I need to take?',
-      answer:
-        'Per the mediation agreement signed on 8/23/24, all affected patients are being notified so that they can make their own choice via the outline above. Both parties request that patients make their choice known by Sept. 20, 2024. If you have already sent in that form, then your choice will be honored by both entities. A link to this form can be found here: <a href="https://pdf.ac/3KY2Y9">https://pdf.ac/3KY2Y9</a>',
-    },
-    {
-      question: 'I was recently seen as a new patient (by either Dr. Gordon or Dr. Keenan) and have yet to matched with any NEDC embryos, what are my options?',
-      answer:
-        'If you wish to be matched with NEDC embryos, then you will remain a patient of Dr. Keenan’s. Contact Dr. Keenan’s new practice and the NEDC. If you are interested in pursuing Embryo Adoption at Rejoice then you can do so with embryos matched to you by either <a href="https://nightlight.org/snowflakes-embryo-adoption-donation/">Snowflakes Embryo Adoption</a> or <a href="https://adoptembryos.org/">Embryo Adoption Services of Cedar Park</a>. Please contact Rejoice at any time if you have any questions.',
-    },
-    {
-      question: 'I am a Rejoice patient (previously seen by Dr. Gordon or Dr. Keenan) but have used all of my NEDC donated embryos, what are my options?',
-      answer: 'Refer to above question for all options available.',
-    },
-    {
-      question: 'Where are my embryos now?',
-      answer:
-        'Your adopted embryos remain where they have always been since they arrived here in Knoxville - safe and sound in the cryostorage tanks (also called dewars) at Rejoice Fertility under the care of our <a href="https://rejoicefertility.com/about-us">experienced embryology team.</a>',
-    },
-    {
-      question: 'When will my embryos be moved if I choose to transfer my care to Dr. Keenan’s new practice?',
-      answer: 'Per the agreement of both parties all of the embryos for patients who wish to continue their care with Dr. Keenan and all of the unmatched embryos will hopefully be moved to Dr. Keenan’s new practice location by October 15, 2024.',
-    },
-    {
-      question: 'When will embryo transfers resume?',
-      answer:
-        'At Rejoice, getting a brand new IVF Laboratory up and running requires diligence and attention to detail. We are scheduling our first group of embryo transfers in the <a href="https://www.facebook.com/story.php?story_fbid=10211808426682206&id=1715810446&mibextid=WC7FNe&rdid=kbiLyaCUi6CHrBOW">new Rejoice Fertility building</a> for the last week of September. During October we will be performing FETs over 3 weeks in order to catch up on patients who have been waiting patiently for their FETs. If you choose to continue your care with Dr. Keenan, please contact his new practice in Powell, TN or the NEDC',
-    },
-    {
-      question: 'What can I do if I have additional questions or concerns?',
-      answer:
-        'Dr. Gordon will continue to make himself available to all patients with questions or concerns. Patients are welcome to contact him via phone, email (<a href="mailto:dr.gordon@rejoiceivf.com">dr.gordon@rejoiceivf.com</a>) or via a video call using the DOXY.ME video conferencing system (please call Taylor or Alyssa at 865-777-0088 to schedule). All of us here at Rejoice are so sorry for any distress that this situation has caused and we are so happy that patients will be able to continue their care with the physician, clinical and embryology team of their choice!',
-    },
   ];
   return (
     <Layout>
